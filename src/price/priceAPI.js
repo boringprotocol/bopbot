4e377373d21f3dcb83a48f0ecbe73992c177f85a
exoboosters <ramesh@exoboosters.tech>
2022-03-21 05:43:32 +0000

    Update priceAPI.js

--- src/price/priceAPI.js
+++ src/price/priceAPI.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 const getSolPriceInUSD = async () => {
-  const json = await fetch('https://api.coingecko.com/api/v3/coins/solana')
+  const json = await fetch('https://api.coingecko.com/api/v3/coins/boring-protocol')
     .then((res) => res.json());
   return json.market_data.current_price.usd;
 };
